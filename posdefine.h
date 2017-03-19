fa7fba8b65a612bb0dba392a91e286c2889e1586
sailorxt <sailorxt@163.com>
2017-03-19 15:30:51 +0800

    数据库管理和3d绘制应当提供的接口

---
+++ posdefine.h
@@ -0,0 +1,12 @@
+#ifndef POSDEFINE
+#define POSDEFINE
+#include <Eigen/Dense>
+#include <vector>
+
+typedef Eigen::Vector2d Pos2d;
+typedef Eigen::Vector3d Pos3d;
+typedef std::vector<Pos2d> Track2d;
+typedef std::vector<Pos3d> Track3d;
+
+#endif // POSDEFINE
+
